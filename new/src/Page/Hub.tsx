eace1df324c2268849c3394201e820968ebee282
jimuk-choi <84140790+jimuk-choi@users.noreply.github.com>
2024-05-19 12:29:11 +0900

    Previous Portfolio Connect

--- new/src/Page/Hub.tsx
+++ new/src/Page/Hub.tsx
@@ -4,6 +4,7 @@ function Hub() {
   return (
     <div>
       <h1>Hub</h1>
+      <Link to="https://jimukchoi.github.io/JMC.github.io/">Previous</Link>
       <Link to="/test01">test01로 이동</Link>
       <Link to="/test02">test02로 이동</Link>
     </div>
