deb785ff7a41d8dd20b4139396f453970c17e775
jimuk-choi <jimook1995@naver.com>
2024-08-30 16:05:29 +0900

    mainPage, Heading Component Test

--- new/src/Page/test01.tsx
+++ new/src/Page/test01.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 import { Button } from "./Hub";
+import {
+  MainSectionTitle,
+  MainSectionTitleWrapper,
+} from "../component_Style/heading/h1";
 
 function Test01() {
   return (
-    <div>
-      <h1>test01 Page</h1>
-      <Link to="/">
+    <>
+      <MainSectionTitleWrapper>
+        <MainSectionTitle>Heading01 Test</MainSectionTitle>
+      </MainSectionTitleWrapper>
+
+      {/* <h1>test01 Page</h1> */}
+      {/* <Link to="/">
         <Button>Hub</Button>
-      </Link>
-    </div>
+      </Link> */}
+    </>
   );
 }
 export default Test01;
---
+++ new/src/component_Style/heading/h1.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const MainSectionTitleWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MainSectionTitle = styled.h1`
+  mix-blend-mode: overlay;
+  font-size: 160px;
+  color: #000000;
+  font-family: "Helvetica Neue-Bold", Helvetica;
+  font-weight: 700;
+  letter-spacing: 12.8px;
+  line-height: normal;
+`;
