4e54e019a6198b37bd59d717a158ef632ff160cb
jimuk-choi <jimook1995@naver.com>
2024-09-04 21:33:06 +0900

    imported video

---
+++ new/src/components/Video.tsx
@@ -0,0 +1,4 @@
+<video muted autoPlay loop>
+  {/* <source src="../src/assets/Video_01.mov" type="video/quicktime" /> */}
+  <source src="Videos/Video_01.mp4" type="video/mp4" />
+</video>;
--- new/src/components/section/main_Greeting_Section.tsx
+++ new/src/components/section/main_Greeting_Section.tsx
@@ -1,6 +1,27 @@
 import { MainSectionTitleWrapper, MainSectionTitle } from "../heading/h1";
 import { useContext } from "react";
 import { LanguageContext } from "../../App"; // 언어 정보를 가져올 컨텍스트
+import styled from "@emotion/styled";
+
+const VideoWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+
+  position: absolute;
+  z-index: -999;
+`;
+const StyledVideo = styled.video`
+  width: 100%;
+  height: auto;
+  /* display: block; */
+`;
+
+const GreetingSection = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 function MainGreetingSection() {
   const { language } = useContext(LanguageContext); // 현재 언어 정보 가져오기
@@ -12,12 +33,20 @@ function MainGreetingSection() {
       : require("../../i18n/kr/main_Section_Greeting.json");
 
   return (
-    <MainSectionTitleWrapper variant="primary">
-      <MainSectionTitle>
-        <p>{jsonData.test}</p>
-        <p>{jsonData.GreetingTitle}</p>
-      </MainSectionTitle>
-    </MainSectionTitleWrapper>
+    <GreetingSection>
+      <VideoWrapper>
+        <StyledVideo autoPlay loop muted>
+          {/* <source src="../src/assets/Video_01.mov" type="video/quicktime" /> */}
+          <source src="Videos/Video_01.mp4" type="video/mp4" />
+        </StyledVideo>
+      </VideoWrapper>
+      <MainSectionTitleWrapper variant="primary">
+        <MainSectionTitle>
+          <p>{jsonData.test}</p>
+          <p>{jsonData.GreetingTitle}</p>
+        </MainSectionTitle>
+      </MainSectionTitleWrapper>
+    </GreetingSection>
   );
 }
 export default MainGreetingSection;
