33637800f34f10a259733b7d0542137633872f04
jimuk-choi <84140790+jimuk-choi@users.noreply.github.com>
2024-09-18 13:57:57 +0900

    header boxshadow added.

--- new/src/components/header/headerStyle.tsx
+++ new/src/components/header/headerStyle.tsx
@@ -31,6 +31,7 @@ export const Header_InnerBox = styled.div<{ isDarkMode: boolean }>`
   background-color: ${(props) =>
     props.isDarkMode ? props.theme.Bg.whiteT20 : props.theme.Bg.whiteT20};
   backdrop-filter: blur(10px);
+  box-shadow: 2px 2px 12px #ededed;
   padding: 0 32px;
 `;
 
