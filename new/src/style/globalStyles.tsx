cd3cdd40c9dff5ddfa81485ca3cddb4cae52ddb0
jimuk-choi <84140790+jimuk-choi@users.noreply.github.com>
2024-09-29 14:49:16 +0900

    test

--- new/src/style/globalStyles.tsx
+++ new/src/style/globalStyles.tsx
@@ -9,8 +9,7 @@ const GlobalStyles = ({ isDarkMode }: GlobalStylesProps) => (
     styles={css`
       @font-face {
         font-family: "Helvetica Neue";
-        src: /* url("/fonts/HelveticaNeue-Regular.woff2") format("woff2"), */ url("../../public/font/HelveticaNeue-Regular.woff")
-          format("woff");
+        src: url("../public/font/HelveticaNeue-Regular.woff") format("woff");
         font-weight: normal;
         font-style: normal;
       }
