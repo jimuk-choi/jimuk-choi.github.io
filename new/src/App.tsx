f816485b7be88052635bf9d0b8826eb0570f9559
jimuk-choi <84140790+jimuk-choi@users.noreply.github.com>
2025-08-18 19:43:37 +0900

    default dark mode changed

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -28,7 +28,7 @@ const LanguageContext = createContext({
 
 function App() {
   const [language, setLanguage] = useState<"en" | "ko">("en");
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
   const [isHovering, setIsHovering] = useState(false);
 
